ec96ad92df0e4e4620d65366763458eb7f53b1e3
Jeff Wear <jeff@mixmax.com>
2015-08-09 20:39:14 -0700

    Re-indent to 2 spaces.

--- Extension/background.js
+++ Extension/background.js
@@ -1,43 +1,43 @@
 chrome.runtime.onInstalled.addListener(function(reason, previousVersion, id) {
-    var reallyCopyImageItemId = "com.jeffreywear.Photocopier.contextMenus.reallyCopyImage";
+  var reallyCopyImageItemId = "com.jeffreywear.Photocopier.contextMenus.reallyCopyImage";
 
-    // Avoid adding a duplicate menu item.
-    chrome.contextMenus.remove(reallyCopyImageItemId, function() {
-        if (chrome.runtime.lastError) {
-            // Ignore the error we'll get when trying to install the item for the
-            // first time, when the item can't be found. We must check the error
-            // or else the runtime will barf.
-        }
+  // Avoid adding a duplicate menu item.
+  chrome.contextMenus.remove(reallyCopyImageItemId, function() {
+    if (chrome.runtime.lastError) {
+      // Ignore the error we'll get when trying to install the item for the
+      // first time, when the item can't be found. We must check the error
+      // or else the runtime will barf.
+    }
 
-        chrome.contextMenus.create({
-            id: reallyCopyImageItemId,
-            title: "Really Copy Image",
-            contexts:["image"]
-        });
-
-        chrome.contextMenus.onClicked.addListener(function(info, tab) {
-            if (info.menuItemId !== reallyCopyImageItemId) return;
-
-            chrome.runtime.sendNativeMessage(
-                "com.jeffreywear.photocopier",
-                {url: info.srcUrl},
-                function(response) {
-                    if (chrome.runtime.lastError) {
-                        console.log('Could not send message to host:', chrome.runtime.lastError);
-                    }
-
-                    // Chrome will call this handler even if the host doesn't
-                    // output anything.
-                    if (!response) return;
-
-                    // The host only outputs errors.
-                    // Note: this will appear in the console of the "background page"
-                    // ("Inspect views: 'background page'" under Photocopier
-                    // on chrome://extensions), not in the console of the page with
-                    // the image.
-                    console.log(response.error);
-                }
-            );
-        });
+    chrome.contextMenus.create({
+      id: reallyCopyImageItemId,
+      title: "Really Copy Image",
+      contexts:["image"]
     });
+
+    chrome.contextMenus.onClicked.addListener(function(info, tab) {
+      if (info.menuItemId !== reallyCopyImageItemId) return;
+
+      chrome.runtime.sendNativeMessage(
+        "com.jeffreywear.photocopier",
+        {url: info.srcUrl},
+        function(response) {
+          if (chrome.runtime.lastError) {
+            console.log('Could not send message to host:', chrome.runtime.lastError);
+          }
+
+          // Chrome will call this handler even if the host doesn't
+          // output anything.
+          if (!response) return;
+
+          // The host only outputs errors.
+          // Note: this will appear in the console of the "background page"
+          // ("Inspect views: 'background page'" under Photocopier
+          // on chrome://extensions), not in the console of the page with
+          // the image.
+          console.log(response.error);
+        }
+      );
+    });
+  });
 });
