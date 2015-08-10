f81146e55dfa1b757ad005bdf737d1713ff190b5
Jeffrey Wear <jeffreywear@gmail.com>
2015-08-09 22:14:02 -0700

    Initial commit

---
+++ ChromeGIFCopier/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ChromeGIFCopier
+//
+//  Created by Jeffrey Wear on 10/1/14.
+//  Copyright (c) 2014 Jeffrey Wear. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
